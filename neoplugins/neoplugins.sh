801518fcab2347a2e2d8bc14b719aaa617cd2b33
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-03-20 18:30:17 -0300

    Fixed Nvimconf

--- neoplugins/neoplugins.sh
+++ neoplugins/neoplugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Plugvim() {
-
+Plugvim(){ 
+    if which "plug.vim">/dev/null; then
     plugvim="sh -c 'curl -O\"${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
     $plugvim
     wait
@@ -13,6 +13,9 @@ Plugvim() {
     dialog --title "Plugvim setup" --msgbox "Vim plug installed" 7 60
     wait
     exit 0
+else
+    dialog --title "Plugvim setup" --msgbox "Vim plug already installed" 7 60
+    fi 
 }
 
 Gitlink(){
@@ -30,33 +33,53 @@ fi
 
 }
 
-
-MenuBox() {
-    dialog --menu "Plugvim helper" 15 50 40 \
-    1 "Setup plugvim" \
-    2 "Install a plugin" \
-    3 "Uninstall a plugin" \
-    4 "Install recommended plugins" \
-    5 "Configure nvim" \
-    6 "Exit" \
+MenuBox(){
+    dialog --menu 'Plugvim helper' 15 50 40 \
+    1 'Setup plugvim' \
+    2 'Install a plugin' \
+    3 'Uninstall a plugin' \
+    4 'Install recommended plugins' \
+    5 'Configure nvim' \
+    6 'Exit' \
     2>&1 >/dev/tty
-}
-$MenuBox
-# echo "$(MenuBox)"
-wait
 
-SelectionInput
+}
+
+Nvimconf(){
+    dialog --menu "Nvim settings" 15 50 40 \
+        1 'Enable numbers' \
+        2 'Enable "cursorline' \
+        3 'Enable syntax' \
+        4 'Change colorscheme' \
+        5 'Manual settings' \
+        6 'Return' \
+        7 'Exit' \
+        2>&1 >/dev/tty
+    }
+
+
 
 while true; do
     MenuChoice=$(MenuBox)
     case $MenuChoice in
         1) Plugvim;;
         2) Gitlink;;
-        3) echo "Uninstall a plugin";;
-        4) echo "Install recommended plugins";;
-        5) echo "Configure nvim";;
+        3) echo 'Uninstall a plugin';;
+        4) echo 'Install recommended plugins';;
+        5) Nvimconf;;
         6) exit 0;;
     esac
-done
+done 
 
-
+    while true; do
+        nvimsetup=$(Nvimconf)
+        case nvimsetup in 
+        1) echo "set numbers" >> $HOME/.config/nvim/init.vim;;
+        2) echo "set cursorline" >> $HOME/.config/nvim/init.vim;;
+        3) echo ":syntax enable" >> $HOME/.config/nvim/init.vim;;
+        4) dialog --title "Colorscheme setup" --msgbox "Not implemented yet" 7 60;;
+        5) nvim $HOME/.config/nvim/init.vim;;
+        6) MenuBox;;
+        7) exit 0;;
+    esac
+        done 
