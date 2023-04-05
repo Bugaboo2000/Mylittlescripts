912a8a7b05171334d84000d26a9d181c991dd4c5
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-04-05 18:30:56 -0300

    Added future condtion for colorschemes

--- neoplugins/neoplugins.sh
+++ neoplugins/neoplugins.sh
@@ -140,3 +140,32 @@ esac
         7) exit 0;;
 esac
     done 
+
+    while true; do
+        colors=$(Colorschemes)
+        case colors in 
+            1) echo ":colorscheme blue";;
+            2) echo ":colorscheme darkblue";;
+            3)echo "";;
+            4)echo "";;
+            5)echo "";;
+            6)echo "";;
+            7)echo"";;
+            8)echo"";;
+            9)echo "";;
+            10)echo "";;
+            11)echo "";;
+            12)echo "";;
+            13)echo "";;
+            14)echo"";;
+            15)echo"";;
+            16)echo"";;
+            17)echo"";; 
+            18)echo"";;
+            19)echo"";;
+            20)echo"";;
+            21)echo "";;
+            22)echo"";;
+            23)exit 0;;
+esac
+    done
