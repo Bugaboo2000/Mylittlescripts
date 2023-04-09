294f44c0837d2e0e044781ed973c97cfd148ccd6
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-04-09 11:54:35 -0300

    Fixed Plugvim and Gitlink if conditions

--- neoplugins/neoplugins.sh
+++ neoplugins/neoplugins.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 Plugvim(){ 
-    if [which "plug.vim">/dev/null]
-    then
+#    if [which "plug.vim">/dev/null]
+#    This is my original solution
+#    Try to fix the condition with this alternative solution:
+    if [-x "$(command -v plug.vim)"] ; then 
     sh -c 'curl -O\"${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     wait
     touch $HOME/.config/nvim/init.vim
     wait
+    mkdir $HOME/.local/share/nvim/site/autoload/plugins/ 
     echo -e "source $HOME/.local/share/nvim/site/autoload/plug.vim\ncall plug#begin('$HOME/.local/share/nvim/site/autoload/plugins')\ncall plug#end()\n" >> $HOME/.config/nvim/init.vim
     wait
     dialog --title "Plugvim setup" --msgbox "Vim plug installed" 7 60
@@ -23,8 +26,9 @@ else
 
 Gitlink(){
 
-if [which "git">/dev/null]
-	then
+# if [which "git">/dev/null]
+# Alternative solution 2
+ if [ -x "$(command -v git)"]; then 
     gitlink=$(dialog --title "Install a Plugin" --inputbox "Paste the github repository" 7 60 2>&1 >/dev/tty)
     cd $HOME/.local/share/nvim/site/autoload/plug.vim && git clone "$gitlink"
     wait
@@ -103,6 +107,10 @@ Nvimconf(){
         23 'Exit' \
         2>&1 > /dev/tty 
 }
+
+# You need to put this conditions inside the variables
+# This solution is dumb like the guy who's code this shittybox
+# Maybe another time i'm gonna reintegrate the conditions inside the variables
 while true; do
     MenuChoice=$(MenuBox)
     case $MenuChoice in
@@ -144,28 +152,28 @@ esac
     while true; do
         colors=$(Colorschemes)
         case colors in 
-            1) echo ":colorscheme blue";;
-            2) echo ":colorscheme darkblue";;
-            3)echo ":colorscheme delek";;
-            4)echo ":colorscheme desert";;
-            5)echo ":colorscheme elford";;
-            6)echo ":colorscheme evening";;
-            7)echo":colorscheme habamax";;
-            8)echo":colorscheme industry";;
-            9)echo ":colorscheme koehler";;
-            10)echo ":colorscheme lunaperche";;
-            11)echo ":colorscheme morning";;
-            12)echo ":colorscheme murphy";;
-            13)echo ":colorscheme pablo";;
-            14)echo":colorscheme peachpuff";;
-            15)echo":colorscheme quiet";;
-            16)echo":colorscheme ron";;
-            17)echo":colorscheme shine";; 
-            18)echo":colorscheme slate";;
-            19)echo":colorscheme torte";;
-            20)echo":colorscheme zellner";;
-            21)echo ":colorscheme default";;
-#            22)dialog --msgbox ;;
+            1) echo ":colorscheme blue" >> $HOME/.config/nvim/init.vim;;
+            2) echo ":colorscheme darkblue" >> $HOME/.config/nvim/init.vim;;
+            3)echo ":colorscheme delek" >> $HOME/.config/nvim/init.vim;;
+            4)echo ":colorscheme desert" >> $HOME/.config/nvim/init.vim;;
+            5)echo ":colorscheme elford" >> $HOME/.config/nvim/init.vim;;
+            6)echo ":colorscheme evening" >> $HOME/.config/nvim/init.vim;;
+            7)echo":colorscheme habamax" >> $HOME/.config/nvim/init.vim;;
+            8)echo":colorscheme industry" >> $HOME/.config/nvim/init.vim;;
+            9)echo ":colorscheme koehler" >> $HOME/.config/nvim/init.vim;;
+            10)echo ":colorscheme lunaperche" >> $HOME/.config/nvim/init.vim;;
+            11)echo ":colorscheme morning" >> $HOME/.config/nvim/init.vim;;
+            12)echo ":colorscheme murphy" >> $HOME/.config/nvim/init.vim;;
+            13)echo ":colorscheme pablo" >> $HOME/.config/nvim/init.vim;;
+            14)echo":colorscheme peachpuff" >> $HOME/.config/nvim/init.vim;;
+            15)echo":colorscheme quiet" >> $HOME/.config/nvim/init.vim;;
+            16)echo":colorscheme ron" >> $HOME/.config/nvim/init.vim;;
+            17)echo":colorscheme shine" >> $HOME/.config/nvim/init.vim;; 
+            18)echo":colorscheme slate" >> $HOME/.config/nvim/init.vim;;
+            19)echo":colorscheme torte" >> $HOME/.config/nvim/init.vim;;
+            20)echo":colorscheme zellner" >> $HOME/.config/nvim/init.vim;;
+            21)echo ":colorscheme default" >> $HOME/.config/nvim/init.vim;;
+            22)dialog --title "Custom colorschmeme" --inputbox "Paste the Github repository" 7 6 0 2>&1 > /dev/tty ;;
             23)exit 0;;
 esac
     done
