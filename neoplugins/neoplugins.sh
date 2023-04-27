755b4ae97efe33fd749ad6d8ec1548fae3ea5eff
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-04-26 21:52:11 -0300

    More improvements

--- neoplugins/neoplugins.sh
+++ neoplugins/neoplugins.sh
@@ -9,7 +9,7 @@ menubox=$(dialog --menu 'Plugvim helper' 15 50 40 \
 4 'Install recommended plugins' \
 5 'Configure nvim manually' \
 6 'Change nvim settings' \
-7 'Exit' 
+7 'Exit' \
 2>&1 >/dev/tty)
 
 case $menubox in 
@@ -21,21 +21,28 @@ case $menubox in
 5) nvim $HOME/.config/nvim/init.vim;;
 6)Nvimconfig;;
 7) exit 0;;
-esac 
-    
+esac
+
 }
+
 PlugInstall() {
 gitlink=$(dialog --title "Install a Plugin" --inputbox "Paste the github repository" 7 60 2>&1 >/dev/tty)
-cd $HOME/.local/share/nvim/site/autoload/plug.vim && git clone "$gitlink"
+cd $HOME/.local/share/nvim/site/autoload/plugins/ && git clone "$gitlink"
+dialog --title "Manual Intervention needed" --msgbox "Done, now point the plugin location and install with :PlugInstall" 7 60
+nvim $HOME/.config/nvim/init.vim
 }
 
 PlugVim() {
 sh -c 'curl -O\"${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim\" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 wait
 touch $HOME/.config/nvim/init.vim
+
 wait
 mkdir $HOME/.local/share/nvim/site/autoload/plugins/
+
 echo -e "source $HOME/.local/share/nvim/site/autoload/plug.vim\ncall plug#begin('$HOME/.local/share/nvim/site/autoload/plugins')\ncall plug#end()\n" >> $HOME/.config/nvim/init.vim
+
 wait
 dialog --title "Plugvim setup" --msgbox "Vim plug installed" 7 60
 
@@ -43,25 +50,14 @@ dialog --title "Plugvim setup" --msgbox "Vim plug installed" 7 60
 
 Plugdel() {
 choice=$(dialog --title "Choose a plugin to Uninstall" --output-fd 1 --dselect $HOME/.local/share/nvim/site/autoload/plugins/ 7 60)
+
 wait
 rm -rf "$choice"
+
 }
 
-Nvimconfig(){
-nvimconf=$(dialog --menu "Nvim settings" 15 50 40 \
-1 'Enable number' \
-2 'Enable "cursorline' \
-3 'Enable syntax' \
-4 'Change colorscheme' \
-5 'Exit' \
-2>&1 >/dev/tty)
-case $nvimconf in
-1) echo "set number" >> $HOME/.config/nvim/init.vim;;
-2) echo "set cursorline" >> $HOME/.config/nvim/init.vim;;
-3) echo ":syntax enable" >> $HOME/.config/nvim/init.vim;;
-5) exit 0;;
-4)$Pallete;;
-esac 
+Nvimconfig() {
+
 Pallete() { colorschemes=$(dialog --menu "Change colorscheme" 15 50 40 \
 1 'Blue' \
 2 'DarkBlue' \
@@ -88,7 +84,7 @@ Pallete() { colorschemes=$(dialog --menu "Change colorscheme" 15 50 40 \
 23 'Exit' \
 2>&1 > /dev/tty)
 
-case $colorschemes in
+case $Pallete in
 
 1) echo ":colorscheme blue" >> $HOME/.config/nvim/init.vim;;
 2) echo ":colorscheme darkblue" >> $HOME/.config/nvim/init.vim;;
@@ -112,10 +108,27 @@ case $colorschemes in
 20)echo":colorscheme zellner" >> $HOME/.config/nvim/init.vim;;
 21)echo ":colorscheme default" >> $HOME/.config/nvim/init.vim;;
 22)dialog --title "Custom colorschmeme" --inputbox "Paste the Github repository" 7 6 0 2>&1 > /dev/tty;;
-23)exit 0;;
+23)exit 0
 esac
+
 }
 
+nvimconf=$(dialog --menu "Nvim settings" 15 50 40 \
+1 'Enable number' \
+2 'Enable "cursorline' \
+3 'Enable syntax' \
+4 'Change colorscheme' \
+5 'Return' \
+6 'Exit' \
+2>&1 >/dev/tty)
+case $nvimconf in
+1) echo "set number" >> $HOME/.config/nvim/init.vim;;
+2) echo "set cursorline" >> $HOME/.config/nvim/init.vim;;
+3) echo ":syntax enable" >> $HOME/.config/nvim/init.vim;;
+4)Pallete;;
+5)Menu;;
+6)exit 0;;
+esac 
 }
 Essentials() {
     while true; do
@@ -123,28 +136,28 @@ Essentials() {
         1 'Ale debugger' \
         2 'fugitive git integration' \
         3 'vim polyglot' \
-        4 'Exit' \
+        4 'Return' \
+        5 'Exit' \
         2>&1 >/dev/tty)
 
         case $autoinstall in
             1)
                 cd "$HOME/.local/share/nvim/site/autoload/plug.vim"
                 git clone https://github.com/dense-analysis/ale.git
-                echo "Plug 'dense-analysis/ale'" >> $HOME/.config/nvim/init.vim 
-                ;;
+                echo "Plug 'dense-analysis/ale'" >> $HOME/.config/nvim/init.vim;;
             2)
                 cd "$HOME/.local/share/nvim/site/autoload/plug.vim"
                 git clone https://github.com/tpope/vim-fugitive.git
-                echo "Plug 'tpope/vim-fugitive'" >> $HOME/.config/nvim/init.vim
-                ;;
+                echo "Plug 'tpope/vim-fugitive'" >> $HOME/.config/nvim/init.vim;;
             3)
                 cd "$HOME/.local/share/nvim/site/autoload/plug.vim"
                 git clone https://github.com/sheerun/vim-polyglot.git
-                echo "Plug 'sheerun/vim-polyglot'" >> $HOME/.config/nvim/init.vim
-                ;;
-            4)exit 0;;
+                echo "Plug 'sheerun/vim-polyglot'" >> $HOME/.config/nvim/init.vim;;
+            4)Menu;;
+
+            5) exit 0;;
 						esac
-							done
+        done
 
 }
 
